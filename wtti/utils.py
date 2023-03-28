8c350fcdb3bc57032b59afd36309d1fdaa9243b9
Keycatowo <owo.nthu@outlook.com>
2023-03-28 13:47:24 +0800

    fix: 時區問題(#1)

--- wtti/utils.py
+++ wtti/utils.py
@@ -22,7 +22,7 @@ def convert_to_timestamp(time_input):
     except (ValueError, TypeError):
         try:
             # 常見的日期時間格式
-            timestamp = datetime.strptime(time_input, "%Y-%m-%d %H:%M:%S")
+            timestamp = datetime.strptime(time_input, "%Y-%m-%d %H:%M:%S").replace(tzinfo=tz)
         except (ValueError, TypeError):
             # 如果都不是上述格式，拋出例外
             raise ValueError("Invalid time format, please use timestamp or '%Y-%m-%d %H:%M:%S' format.")
