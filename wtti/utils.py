0a7263144493e1faa466ef1f5e706aa9fe48235c
Keycatowo <owo.nthu@outlook.com>
2023-03-28 13:57:16 +0800

    fix: 設時區(#1)

--- wtti/utils.py
+++ wtti/utils.py
@@ -23,6 +23,8 @@ def convert_to_timestamp(time_input):
         try:
             # 常見的日期時間格式
             timestamp = datetime.strptime(time_input, "%Y-%m-%d %H:%M:%S").replace(tzinfo=tz)
+            timestamp = timestamp.astimezone(tz)
+            
         except (ValueError, TypeError):
             # 如果都不是上述格式，拋出例外
             raise ValueError("Invalid time format, please use timestamp or '%Y-%m-%d %H:%M:%S' format.")
