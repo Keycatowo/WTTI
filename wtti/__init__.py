4a33f262b48b62b4ce5b1291ccfdf85fbc0b6890
Keycatowo <owo.nthu@outlook.com>
2023-03-28 15:00:54 +0800

    feat: 新增版本確認

--- wtti/__init__.py
+++ wtti/__init__.py
@@ -2,3 +2,7 @@ __version__ = "0.1.0"
 
 from .Content import Content # noqa: F401
 from .Text import Text # noqa: F401
+
+
+from .version_check import check_new_version # noqa: F401
+check_new_version()
